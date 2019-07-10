31d6477831c741bdbd7db96efe83d9342e555101
ginzbeda <dginzberg@umass.edu>
2019-07-09 22:39:03 -0400

    07/09/19 SpotifySDK up to Auth

--- app/src/main/java/com/example/biometricprofile/MainActivity.java
+++ app/src/main/java/com/example/biometricprofile/MainActivity.java
@@ -39,6 +39,7 @@ import static com.example.biometricprofile.Profile.setAccel;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+
     private static final String TAG = "MainActivity";
     private SensorManager manager;
     private SensorEventListener listener;
